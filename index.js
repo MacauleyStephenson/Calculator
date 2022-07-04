ea62f352796d0c584868e412456a112a787304c2
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-07-04 14:07:45 +0100

    implimented clear inputs with consturctors

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement
+		this.currentOperandTextElement = currentOperandTextElement
+		this.clear()
+	}
+}
+
+clear(){
+
+}
+
+delete (){
+
+}
+
+appendNumber(number){
+
+}
+
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+updateDisplay() {
+}
